4847445f349d5a32e295400f2e3556e00a4f4b85
Janosch Püschel <janosch.pueschel@gmail.com>
2026-09-11 15:00:56 +0200

    feat: add route for BookDetailPage to display detailed information for individual books

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,10 +1,13 @@
-import { Routes, Route } from "react-router-dom";
-import BooksPage from "../features/books/BooksPage";
+import { Route, Routes } from "react-router-dom";
+
+import BookDetailPage from "../features/books/pages/BookDetailPage";
+import BooksPage from "../features/books/pages/BooksPage";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="books" element={<BooksPage />} />
+      <Route path="books/:bookId" element={<BookDetailPage />} />
     </Routes>
   );
 }
